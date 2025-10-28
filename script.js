a1987683e77792ff6986cdf2461d5586034fc9dd
simonestrinati <simone.strinati@outlook.it>
2025-10-28 18:11:21 +0100

    creazione input

---
+++ script.js
@@ -0,0 +1,14 @@
+//intro
+
+const inputKm = prompt("Inserisci chilometraggio");
+const discountUnder = prompt("Inserisci sconto under18");
+const discountOver = prompt("Inserisci sconto over65");
+const enterAge = prompt("Inserisci età")
+let result = null;
+
+//svolgimento
+
+
+
+//conclusione
+
